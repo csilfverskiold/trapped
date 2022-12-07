b19a333f002c284ef13c58867f8806ccd653f6b7
csilfverskiold <christian.silfverskiold@gmail.com>
2022-12-07 10:27:40 +0000

    Create game intro function

---
+++ run.py
@@ -0,0 +1,13 @@
+def game_intro():
+    """
+    Game introduction and opening scene requesting data input
+    """
+    print("-^-^-^- Trapped! -^-^-^-")
+    print("--This placeholder text will introduce the player")
+    print("to the game and let them start the game by requested")
+    print("input data.--")
+
+    input("Press 1 and ENTER to move forward: ")
+
+
+game_intro()
