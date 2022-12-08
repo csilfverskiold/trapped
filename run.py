a84551e7f83d9c0683a11e8c6e69c60c6a44cd2e
csilfverskiold <christian.silfverskiold@gmail.com>
2022-12-08 09:56:19 +0000

    Add trap 1-5 functions

--- run.py
+++ run.py
@@ -22,4 +22,79 @@ def game_intro():
             print("Value must be whole number, hit 1 and ENTER")
 
 
+def trap_1():
+    """
+    DOCSTRING for TRAP 1
+    """
+    print("")
+    print(">>> Placeholder text for TRAP 1 telling player about two")
+    print("options to choose. <<<\n")
+    print("1 - Choose this option")
+    print("2 - Choose that option")
+    print("")
+
+    input("Enter choice: ")
+
+
+def trap_2():
+    """
+    DOCSTRING for TRAP 2
+    """
+    print("")
+    print(">>> Placeholder text for TRAP 2 telling player about two")
+    print("options to choose. <<<\n")
+    print("1 - Choose this option")
+    print("2 - Choose that option")
+    print("")
+
+    input("Enter choice: ")
+
+
+def trap_3():
+    """
+    DOCSTRING for TRAP 3
+    """
+    print("")
+    print(">>> Placeholder text for TRAP 3 telling player about two")
+    print("options to choose. <<<\n")
+    print("1 - Choose this option")
+    print("2 - Choose that option")
+    print("")
+
+    input("Enter choice: ")
+
+
+def trap_4():
+    """
+    DOCSTRING for TRAP 4
+    """
+    print("")
+    print(">>> Placeholder text for TRAP 4 telling player about two")
+    print("options to choose. <<<\n")
+    print("1 - Choose this option")
+    print("2 - Choose that option")
+    print("")
+
+    input("Enter choice: ")
+
+
+def trap_5():
+    """
+    DOCSTRING for TRAP 5
+    """
+    print("")
+    print(">>> Placeholder text for TRAP 5 telling player about two")
+    print("options to choose. <<<\n")
+    print("1 - Choose this option")
+    print("2 - Choose that option")
+    print("")
+
+    input("Enter choice: ")
+
+
 game_intro()
+trap_1()
+trap_2()
+trap_3()
+trap_4()
+trap_5()
